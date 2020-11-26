ec38ee454273aad997dca595abecf9565bbdc6de
Jim Publicover <jameslpublicover@gmail.com>
2020-11-26 11:42:36 +0100

    devise, bootstrap, rubocop

---
+++ app/controllers/feelings_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class FeelingsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index; end
+
+  def show; end
+
+  def new; end
+
+  def edit; end
+
+  def create; end
+
+  def update; end
+
+  def destroy; end
+end
